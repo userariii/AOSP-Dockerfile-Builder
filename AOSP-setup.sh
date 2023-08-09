2e71baeb09eda0fe468207eb754347e59b658bf6
ARINDAM BHATTACHARJEE <abhattacharjee717@gmail.com>
2023-08-10 04:38:17 +0530

    Builder: Add AOSP-setup.sh
    This will install Docker engine & run the Dockerfile to create an image & the container.

---
+++ AOSP-setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Update the apt package index and install packages to allow apt to use a repository over HTTPS:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+
+# Add Docker’s official GPG key:
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Use the following command to set up the repository:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update the apt package index:
+sudo apt-get update
+
+# Install Docker Engine, containerd, and Docker Compose.
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Create the docker group.
+sudo groupadd docker
+
+# Add your user to the docker group.
+sudo usermod -aG docker $USER
+
+# Configure Docker to start on boot with systemd
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+# Build Docker image from the Dockerfile [Image name: aosp-builder]
+docker build -t aosp-builder .
+
+# Make a container with the build image [Container name: aosp-container]
+docker run -it --name aosp-container aosp-builder
